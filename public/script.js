e5f0c1ef3ed838574b1f3e80e2559f27a32ca280
honnnei <hannpop@gmail.com>
2020-04-01 00:31:35 +0200

    google search works very basic single result

---
+++ public/script.js
@@ -0,0 +1,155 @@
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("Testing DOM");
+    let topTenButtonClick = false;
+    let luckyButtonClick = false;
+
+
+    $("#topTenResultsButton").click(function(e) {
+        event.preventDefault();
+        topTenButtonClick = true;
+        getQuery();
+    });
+
+    $("#luckyResultButton").click(function(e) {
+        event.preventDefault();
+        luckyButtonClick = true;
+        getQuery();
+
+    });
+
+
+    function getQuery() {
+        if (topTenButtonClick) {
+            //show top ten results
+            let resultsQuery = $("#searchBox").val();
+            console.log("will show results for: " + resultsQuery);
+            showTopTenResults(resultsQuery);
+        } else if (luckyButtonClick) {
+            //show lucky result
+            let luckyQuery = $("#searchBox").val();
+            console.log("will show one lucky result for: " + luckyQuery);
+            showLuckyResult(luckyQuery);
+        }
+        else {
+            console.log('error');
+        }
+    }
+
+    function showTopTenResults(resultsQuery) {
+                axios.get(`https://www.googleapis.com/customsearch/v1?key=AIzaSyBZSy_eMGwVqsKZzHBPvIiBz-HBFmK8Uxo&cx=006369568077011760523:re8fjbecuqp&q=${resultsQuery}`)
+                .then(function (googleResults) {
+                    let results = [];
+                    results = googleResults.data.items
+                    // let title1 = results[0].title;
+                    // console.log(title1);    
+                    // return results; 
+                    // getTitleSnippetUrl(results);
+                    // console.log(results);
+                    let title = results[0].title;
+                    let snippet = results[0].snippet;
+                    let link = results[0].link;
+                    console.log(link);
+                    displayResult(title, snippet, link);
+                       
+                });
+    }
+
+    function displayResult(titleOf, snippetOf, linkOf) {
+        $(".link").html(titleOf);
+        $(".snippet").html(snippetOf);
+        // $(".link").html("link");
+        $(".link").attr("href", linkOf);
+        
+    }
+    
+    function getTitleSnippetUrl(argument) {
+            argument.map(item => {
+                console.log(item.title);
+                console.log(item.snippet);
+                console.log(item.link);
+            });
+    }
+
+    function showLuckyResult(luckyQuery) {
+
+        axios.get(`https://www.googleapis.com/customsearch/v1?key=AIzaSyBZSy_eMGwVqsKZzHBPvIiBz-HBFmK8Uxo&cx=006369568077011760523:re8fjbecuqp&q=${luckyQuery}`)
+                .then(function (result){
+                    let results = [];
+                    results = result.data.items
+                    return results; 
+                       
+                });
+
+    }
+    // function gettingTopTenSearchQuery() {
+    //     $("#topTenResultsButton").click(function(e) {
+    //         event.preventDefault();
+    //         let resultsQuery = $("#searchBox").val();
+    //         console.log(resultsQuery);
+        
+    //     });
+    // }
+
+    // gettingTopTenSearchQuery();
+
+    // function gettingLuckyResultSearchQuery() {
+    //     $("#luckyResultButton").click(function(e) {
+    //         event.preventDefault();
+    //         let luckyQuery = $("#searchBox").val();
+    //         console.log(luckyQuery);
+        
+    //     });
+    // }
+
+    // gettingLuckyResultSearchQuery();
+
+    // $(function() {
+    //     $("#topTenResultsButton").click(function(e) {
+    //         event.preventDefault();
+    //         let search = $("#searchBox").val();
+    //         console.log(search);
+        
+    //         // if (($("#rsvp").val()) === "yes") {
+    //         //     console.log("if works");
+    //         // }
+    //     });
+    // });
+        
+});
+
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The dom has loaded hp");
+
+//     let searchBox = $("#form").val();
+//     let searchButton = $("#topTenResults");
+   
+    
+// });
+
+// searchButton.addEventListener('click', function() {
+//     event.preventDefault();
+//     console.log(searchBox.value);
+// });
+
+// // let searchButton = document.getElementById("topTenResults");
+// // // .addEventListener("click", topTenResults);
+// // let luckyButton = document.getElementById("luckyResult");
+// // // .addEventListener("click", luckyResult);
+// // let searchBox= document.getElementById("form").value;
+
+
+// // searchButton.addEventListener('click', function() {
+// //     event.preventDefault();
+// //     console.log(searchBox.value);
+// // });
+
+// // function topTenResults() {
+// //      console.log('top ten');
+// // }
+    
+// // function luckyResult() {
+// //     console.log('lucky');
+// // }
+
